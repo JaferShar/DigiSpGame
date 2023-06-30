000de82a0f5cf8c5b94b96d34d0ddb6e93d61caa
JaferShar <jafer.shar@gmail.com>
2023-07-01 01:59:04 +0200

    Aufgabe 2 semi fertig :D

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Teleport : MonoBehaviour
 {
 
-    public Transform tpPoint;
+    public Transform tpPoint1;
+    public Transform tpPoint2;
     // Start is called before the first frame update
     private void OnCollisionEnter(Collision other) {
         if (other.gameObject.name.Equals("Plattform")) {
-            gameObject.transform.position = tpPoint.position;
+            gameObject.transform.position = tpPoint1.position;
+            Physics.SyncTransforms();
+        } else if (other.gameObject.name.Equals("Plattform2")) {
+            gameObject.transform.position = tpPoint2.position;
             Physics.SyncTransforms();
         }
     }
